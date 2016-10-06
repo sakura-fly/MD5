a0cc07335c10e91156472c1f8f0a42127d0911af
赵明宇 <498973030@qq.com>
2016-10-06 23:57:43 +0800

    init

---
+++ src/io/github/zhaomy6/Main.java
@@ -0,0 +1,7 @@
+package io.github.zhaomy6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MD5.md5("The quick brown fox jumps over the lazy dog"));
+    }
+}
